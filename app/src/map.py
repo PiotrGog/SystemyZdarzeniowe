990bd1ecc3d54afa6ce7fc01392b868bb66b087d
Piotr Gogola <gogola.piotr@interia.pl>
2019-11-21 10:30:07 +0100

    consts updated

---
+++ app/src/map.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr = np.array([[[0, 1],
+                 [1, 0]],
+
+                [[0, 1],
+                 [1, 0]]])
+print(arr.shape)
---
+++ app/src/robot_driver.py
@@ -0,0 +1,6 @@
+from .consts import (
+    MapObject,
+    RobotNotification,
+    RobotStatus
+)
+
